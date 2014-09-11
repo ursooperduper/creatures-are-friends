c9ae5f08ca26189a5ef734dd1c55f9a124978af2
Sarah Kuehnle <sarah@sooperduper.com>
2014-09-11 14:55:36 -0400

    Initial Commit

---
+++ Creatures are Friends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Creatures are Friends
+//
+//  Created by Sarah Kuehnle on 9/11/14.
+//  Copyright (c) 2014 Sarah Kuehnle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Creatures are FriendsTests/Creatures_are_FriendsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Creatures_are_FriendsTests.swift
+//  Creatures are FriendsTests
+//
+//  Created by Sarah Kuehnle on 9/11/14.
+//  Copyright (c) 2014 Sarah Kuehnle. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Creatures_are_FriendsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
