676a904a0a1c424d8962a8ff4da7e690231299fb
Sarah Kuehnle <sarah@sooperduper.com>
2014-09-22 17:56:40 -0400

    Camera button has been hooked up and the snapped pic is saving to the photo library. The next step will be getting the photo to load in the edit screen.

--- Creatures are Friends/Menu.swift
+++ Creatures are Friends/Menu.swift
@@ -8,16 +8,68 @@
 
 import UIKit
 
-class Menu: UIViewController {
+class Menu: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // ----------------------------- Button Actions -----------------------------
+    
+    @IBOutlet var btnCamera: UIImageView!
+    @IBOutlet var btnChoosePic: UIImageView!
+    @IBOutlet var btnViewCreatures: UIImageView!
+    
+    // ----------------------------- Gestures -----------------------------
+    
+    @IBOutlet var gestureTapCamera: UITapGestureRecognizer!
+    @IBAction func handleTapCamera(sender: UITapGestureRecognizer) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            // Load the camera interface
+            let picker = UIImagePickerController()
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            picker.delegate = self
+//            picker.allowsEditing = false
+            
+            // Show the camera interface
+            self.presentViewController(picker, animated: true, completion: nil)
+            
+        } else {
+            // There is no camera vailable
+            let alert = UIAlertController(title: "Error", message: "There is no camera available.", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Yes", style: .Default, handler: {(alertAction) in
+                alert.dismissViewControllerAnimated(true, completion: nil)
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBOutlet var gestureTapChoosePic: UITapGestureRecognizer!
+    @IBAction func handleTapChoosePic(sender: UITapGestureRecognizer) {
+        println("Choose a picture")
+    }
+    
+    @IBOutlet var gestureTapViewPics: UITapGestureRecognizer!
+    @IBAction func handleTapViewPics(sender: UITapGestureRecognizer) {
+        println("View your creatures")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        btnCamera.addGestureRecognizer(gestureTapCamera)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        println("User wants to save the photo")
+        
+        let imageToSave: UIImage = info[UIImagePickerControllerOriginalImage] as UIImage
+        UIImageWriteToSavedPhotosAlbum(imageToSave, nil, nil, nil)
+        
+        
+        // Instead of this, transition to the Photo Edit view with the photo taken in the editor
+        picker.dismissViewControllerAnimated(true, completion: nil)
     }
 }
