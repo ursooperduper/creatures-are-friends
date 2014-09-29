9485f08e43d7a6197e9b1afa6f5b81ffe513095a
Sarah Kuehnle <sarah@sooperduper.com>
2014-09-29 14:56:18 -0400

    Refactored the big getcombinedImage function and split it in two.

--- Creatures are Friends/PhotoEdit.swift
+++ Creatures are Friends/PhotoEdit.swift
@@ -12,137 +12,89 @@ import CoreImage
 import CoreGraphics
 
 class PhotoEdit: UIViewController, UIGestureRecognizerDelegate {
-    
-    // Number of character images in the library
+
     let characterCount = 20
-    
-    // Photos Framework
     var assetCollection: PHAssetCollection!
     var photos: PHFetchResult!
     var index: Int!
-    
-    // Information about the character
     var characterInfo = [ String: CGFloat]()
     var currScale: CGFloat = 1
     var currRotation: CGFloat = 0
     var characterPos: CGPoint = CGPointZero
 
-    @IBOutlet var imgContainer: UIView! // The View that contains the photo and character
-    @IBOutlet var photoImg: UIImageView! // The main photo
-    @IBOutlet var characterImg: UIImageView! // The character head
+    @IBOutlet var imgContainer: UIView!
+    @IBOutlet var photoImg: UIImageView!
+    @IBOutlet var characterImg: UIImageView!
 
-    // Creates a new photo out of the combined character and photo images
-    func getCombinedImage(topImg: UIImage, bottomImg: UIImage) -> UIImage {
-        
-        // Create CIImage versions of the top & bottom images
-        let fgImg = CIImage(image: topImg)
-        let bgImg = CIImage(image: bottomImg)
-        
-        // Define the affine transformations
-        let makeRotation = CGAffineTransformMakeRotation(-(characterInfo["rotation"]!))
-        
-        // Dividing by 2 makes the head appear correctly on iPhone 5s. Need to test on other devices.
-        let makeScale = CGAffineTransformMakeScale((characterInfo["scale"])!/2, (characterInfo["scale"])!/2)
-        
-        // Create an affine transformation matrix
-        let affineConcat = CGAffineTransformConcat(makeScale, makeRotation)
-        
-        // Define the context
-        let affineContext = CIContext(options: nil)
-        
-        // Create the affine filter
-        let affineFilter: CIFilter = CIFilter(name: "CIAffineTransform")
-        affineFilter.setValue(fgImg, forKey: "inputImage")
-        affineFilter.setValue(NSValue(CGAffineTransform: affineConcat), forKey: "inputTransform")
-        
-        // Grab the resulting image data
-        let affineResult = affineFilter.valueForKey("outputImage") as CIImage
-        
-        // Define the size of the final image, this is the size of the photo
-        let photoSize = CGSizeMake(bottomImg.size.width, bottomImg.size.height)
-        
-        // Begin the image context
+    func formatCharacterForSaving(character: UIImage) -> UIImage {
+        let rotation   = CGAffineTransformMakeRotation(-(characterInfo["rotation"]!))
+        let scale = CGAffineTransformMakeScale((characterInfo["scale"])!/2, (characterInfo["scale"])!/2)
+        let affineTransformationMatrix = CGAffineTransformConcat(rotation, scale)
+        let context = CIContext(options: nil)
+        let transformFilter = CIFilter(name: "CIAffineTransform")
+        let characterData = CIImage(image: character)
+        transformFilter.setValue(characterData, forKey: "inputImage")
+        transformFilter.setValue(NSValue(CGAffineTransform: affineTransformationMatrix), forKey: "inputTransform")
+        let filterResult = transformFilter.valueForKey("outputImage") as CIImage
+        return UIImage(CIImage: filterResult)!
+    }
+    
+    func getCombinedImage(character: UIImage, photo: UIImage) -> UIImage {
+        let photoSize = CGSizeMake(photo.size.width, photo.size.height)
         UIGraphicsBeginImageContext(photoSize)
-        
-        // Add the photo to the context
-        bottomImg.drawInRect(CGRectMake(0, 0, photoSize.width, photoSize.height))
-        
-        // Set up the details for the new character image
-        let newFgImg = UIImage(CIImage: affineResult)
-        let newFgImgWidth = newFgImg?.size.width
-        let newFgImgHeight = newFgImg?.size.height
-        
-        newFgImg?.drawInRect(CGRectMake(characterInfo["xPos"]!, characterInfo["yPos"]!, newFgImgWidth!, newFgImgHeight!), blendMode: kCGBlendModeNormal, alpha: 1.0)
-        
-        // Grab the graphic from the image context
-        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()
-        
-        // End the context
+        photo.drawInRect(CGRectMake(0, 0, photoSize.width, photoSize.height))
+        character.drawInRect(CGRectMake(characterInfo["xPos"]!, characterInfo["yPos"]!, character.size.width, character.size.height), blendMode: kCGBlendModeNormal, alpha: 1.0)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
         return newImage
     }
 
-    // Returns a random integer between two specified numbers
     func randomInt(min: Int, max: Int) -> Int {
         return min + Int(arc4random_uniform(UInt32(max - min + 1)))
     }
 
-    // Retrieves an image using PHImageManager, the target size is of the screen dimensions.
     func displayPhoto() {
         let imageManager = PHImageManager.defaultManager()
         let optionsForImage = PHImageRequestOptions()
         optionsForImage.resizeMode = .Exact
         optionsForImage.synchronous = true
-        
+
         let id = imageManager.requestImageForAsset(self.photos[self.index] as PHAsset, targetSize: CGSize(width: imgContainer.bounds.width, height: imgContainer.bounds.height), contentMode: .AspectFill, options: optionsForImage, resultHandler: {(result, info) in
             self.photoImg.image = result
         })
     }
 
-    // Adds the new photo to the Creatures are Friends library
     func addNewAssetWithImage(image: UIImage, toAlbum album:PHAssetCollection) {
         PHPhotoLibrary.sharedPhotoLibrary().performChanges({
-            // Request creating an asset from the image.
             let createAssetRequest = PHAssetChangeRequest.creationRequestForAssetFromImage(image)
-
-            // Request editing the album.
             let albumChangeRequest = PHAssetCollectionChangeRequest(forAssetCollection: album)
-
-            // Get a placeholder for the new asset and add it to the album editing request.
             let assetPlaceholder = createAssetRequest.placeholderForCreatedAsset
             albumChangeRequest.addAssets([assetPlaceholder])
-
             }, completionHandler: { success, error in
                 NSLog("Finished adding asset. %@", (success ? "Success" : error))
         })
     }
 
-    // ------------------------------ Button Actions ------------------------------
-    
-    // Cancel button
     @IBAction func btnCancel(sender: AnyObject) {
         self.navigationController?.popToRootViewControllerAnimated(true)
     }
-    
-    // Export/save button
+
     @IBAction func btnExport(sender: AnyObject) {
         characterInfo["xPos"] = characterImg.frame.origin.x
         characterInfo["yPos"] = characterImg.frame.origin.y
         characterInfo["scale"] = currScale
         characterInfo["rotation"] = currRotation
-
-        let imageToSave = getCombinedImage(self.characterImg.image!, bottomImg: self.photoImg.image!)
+        
+        let character = formatCharacterForSaving(self.characterImg.image!)
+        let imageToSave = getCombinedImage(character, photo: self.photoImg.image!)
         addNewAssetWithImage(imageToSave, toAlbum: self.assetCollection)
 
         self.navigationController?.popToRootViewControllerAnimated(true)
     }
 
-    // ** GESTURES **
-    // Gesture: Double Tap
     @IBOutlet var gestureDoubleTap: UITapGestureRecognizer!
     @IBAction func handleGestureDoubleTap(recognizer: UITapGestureRecognizer) {
-        
+
         let location = recognizer.locationInView(self.view)
         characterImg.hidden = false
         characterImg.center = location
@@ -152,8 +104,6 @@ class PhotoEdit: UIViewController, UIGestureRecognizerDelegate {
         characterImg.image = character
     }
 
-    // Gesture: Pan
-    // Allow the user to drag the character around the screen.
     @IBOutlet var gesturePanCharacter: UIPanGestureRecognizer!
     @IBAction func handleGesturePanCharacter(recognizer: UIPanGestureRecognizer) {
         let translation = recognizer.translationInView(self.view)
@@ -161,8 +111,7 @@ class PhotoEdit: UIViewController, UIGestureRecognizerDelegate {
             y:recognizer.view!.center.y + translation.y)
         recognizer.setTranslation(CGPointZero, inView: self.view)
     }
-    
-    // Gesture: Pinch (to scale)
+
     @IBOutlet var gesturePinchCharacter: UIPinchGestureRecognizer!
     @IBAction func handleGesturePinchCharacter(recognizer: UIPinchGestureRecognizer) {
         recognizer.view!.transform = CGAffineTransformScale(recognizer.view!.transform, recognizer.scale, recognizer.scale)
@@ -170,7 +119,6 @@ class PhotoEdit: UIViewController, UIGestureRecognizerDelegate {
         recognizer.scale = 1
     }
 
-    // Gesture: Rotate
     @IBOutlet var gestureRotateCharacter: UIRotationGestureRecognizer!
     @IBAction func handleGestureRotateCharacter(recognizer: UIRotationGestureRecognizer) {
         recognizer.view!.transform = CGAffineTransformRotate(recognizer.view!.transform, recognizer.rotation)
@@ -178,10 +126,8 @@ class PhotoEdit: UIViewController, UIGestureRecognizerDelegate {
         recognizer.rotation = 0
     }
 
-    // Controller override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Create instances of the gesture recognizers
         view.addGestureRecognizer(gestureDoubleTap)
         characterImg.addGestureRecognizer(gesturePanCharacter)
         characterImg.addGestureRecognizer(gesturePinchCharacter)
@@ -203,7 +149,6 @@ class PhotoEdit: UIViewController, UIGestureRecognizerDelegate {
         return true
     }
 
-    // When the camera is shaken, change the character image
     override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
         if(event.subtype == UIEventSubtype.MotionShake) {
             var character = UIImage(named: "head_" + String(randomInt(1, max: characterCount)))
@@ -215,8 +160,6 @@ class PhotoEdit: UIViewController, UIGestureRecognizerDelegate {
         super.didReceiveMemoryWarning()
     }
 
-    // ------------------------------ Optional method implementations ------------------------------
-    // Implement gesture delegate recognizer optional function to allow the user to perform multiple gestures
     func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
         return true
     }
